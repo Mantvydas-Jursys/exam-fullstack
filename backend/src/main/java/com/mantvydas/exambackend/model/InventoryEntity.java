71fb5869134b4f224f1c480c5dbe083ed71c2513
Mantvydas Jursys <mantvydas.jursys@stud.vtmc.lt>
2023-02-27 17:28:48 +0200

    added inventoryEntity

--- backend/src/main/java/com/mantvydas/exambackend/model/InventoryEntity.java
+++ backend/src/main/java/com/mantvydas/exambackend/model/InventoryEntity.java
@@ -1,12 +1,35 @@
 package com.mantvydas.exambackend.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class InventoryEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@NotBlank
+	private String name;
+	
+	@NotBlank
+	private Double mass;
+	
+	@Min(value=1)
+	@Max(value=40)
+	private Integer sector;
+	
+	@CreatedDate
+    private LocalDateTime createdDate;
 	
 }
