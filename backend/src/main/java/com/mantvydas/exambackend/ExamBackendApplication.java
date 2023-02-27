224d711e4cd688f1d859a92527abb4d110ed5b05
Mantvydas Jursys <mantvydas.jursys@stud.vtmc.lt>
2023-02-27 17:23:23 +0200

    added cors filter

--- backend/src/main/java/com/mantvydas/exambackend/ExamBackendApplication.java
+++ backend/src/main/java/com/mantvydas/exambackend/ExamBackendApplication.java
@@ -2,13 +2,24 @@ package com.mantvydas.exambackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExamBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamBackendApplication.class, args);
-		
+
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
---
+++ backend/src/main/java/com/mantvydas/exambackend/model/InventoryEntity.java
@@ -0,0 +1,12 @@
+package com.mantvydas.exambackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class InventoryEntity {
+
+	@Id
+	private long id;
+	
+}
